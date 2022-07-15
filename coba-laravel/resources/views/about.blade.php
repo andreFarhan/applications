920a924dd1a0140189b608cd3bd9c494cae3f032
andreFarhan <andrefarhansaputra@gmail.com>
2022-07-15 22:43:26 +0700

    1

---
+++ coba-laravel/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>UNPAM | ABOUT</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>Selamat Datang</h1>
+	<p><?= $nama; ?>	</p>
+	<p><?= $email; ?></p>
+	<p><img src="gambar/<?= $gambar; ?>" alt="<?= $nama; ?>" width="200"></p>
+	<script src="js/script.js"></script>
+</body>
+</html>
